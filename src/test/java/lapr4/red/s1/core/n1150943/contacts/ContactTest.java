cc66d6d40f2ef52f394ae70f003c09bb4a749d85
João Cardoso <1150943@isep.ipp.pt>
2017-05-31 23:05:11 +0100

    LAPR4E17DD-109 # Implemented Unit Tests for domain classes used in Use Case

--- src/test/java/lapr4/red/s1/core/n1150943/contacts/ContactTest.java
+++ src/test/java/lapr4/red/s1/core/n1150943/contacts/ContactTest.java
@@ -4,6 +4,11 @@ import lapr4.white.s1.core.n4567890.contacts.domain.Contact;
 import org.junit.Before;
 import org.junit.Test;
 
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+
 /**
  * Unit Test class to test Contact
  *  @see Contact
@@ -16,27 +21,45 @@ public class ContactTest {
     String last = "doe";
     String name = "john doe";
 
+    Contact contact1=null;
+    Contact contact2=null;
+
     public ContactTest() {
     }
 
     @Before
     public void setUp() {
-
+        contact1 = new Contact("João Cardoso", "João","Cardoso","");
+        contact2 = new Contact(name,first,last,"");
     }
 
     @Test(expected = IllegalStateException.class)
     public void testContactHasName() {
-        Contact c = new Contact("",first,last);
+        Contact c = new Contact("",first,last,"");
     }
 
     @Test(expected = IllegalStateException.class)
     public void testContactHasFirstName() {
-        Contact c = new Contact(name,"",last);
+        Contact c = new Contact(name,"",last,"");
     }
 
     @Test(expected = IllegalStateException.class)
     public void testContactHasLastName() {
-        Contact c = new Contact(name,first,"");
+        Contact c = new Contact(name,first,"","");
     }
+
+    @Test
+    public void testContactIsSameTrue() {
+        Contact c = new Contact(name,first,last,"");
+        boolean res = contact2.sameAs(c);
+        assertTrue(res);
+    }
+
+    @Test
+    public void testContactIsSameFalse() {
+        boolean res = contact2.sameAs(contact1);
+        assertFalse(res);
+    }
+
 
 }
