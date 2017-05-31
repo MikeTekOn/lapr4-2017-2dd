6bdb4aeac2a4a6a863bd82806d12ae780cdf0ef9
Renato <1140822@isep.ipp.pt>
2017-05-31 17:45:50 +0100

    LAPR4E17DD-111 #comment adds domain junit testing,implements beanshellisntance class, beanshellloaderclass,updates uc artifacts

---
+++ src/main/java/lapr4/blue/s1/lang/n1140822/beanshellwindow/BeanShellLoader.java
@@ -0,0 +1,52 @@
+/**
+ * Package location for 1140822's code related to beanshell integration.
+ */
+package lapr4.blue.s1.lang.n1140822.beanshellwindow;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+/**
+ * @author Renato Oliveira 1140822@isep.ipp.pt
+ */
+public class BeanShellLoader {
+
+    /**
+     * String that has the script name information to build - the script.
+     */
+    private String scriptName;
+
+    /**
+     * The newly built beanshell instance.
+     */
+    private BeanShellInstance instance;
+
+    public BeanShellLoader() {
+
+    }
+
+    /**
+     * Creates the class using the given script.
+     *
+     * @param scriptName the script
+     * @return the class instance
+     */
+    public BeanShellInstance create(String scriptName) throws FileNotFoundException {
+        this.scriptName = scriptName;
+        LinkedList<String> code = new LinkedList<>();
+        Scanner scan = new Scanner(new File(scriptName));
+        while(scan.hasNextLine())
+        {
+            code.add(scan.nextLine());
+        }
+        if(code.isEmpty())
+        {
+            throw new IllegalStateException("Cannot create script without any code.");
+        }
+        instance = new BeanShellInstance(code);
+        return instance;
+    }
+
+}
