75c1717dd85f00712eceed7c1493aeae06780179
Miguel Silva <1150901@isep.ipp.pt>
2017-06-01 15:37:16 +0100

    LAPR4E17DD-100 #comment I've corrected some things on the analysis including both Puml in there.

--- src/main/java/lapr4/green/s1/ipc/n1150901/search/workbook/package-info.java
+++ src/main/java/lapr4/green/s1/ipc/n1150901/search/workbook/package-info.java
@@ -29,13 +29,31 @@
  * <b>Analysis SD:</b><p>
  * <img src="us081_analysis.png" alt="analysis">
  * <p>
- * This is a very early analysis of what this user story is supposed to be.
- * It main contain incoherences that can be corrected when a further investigation
- * of the code is performed. The development of the design can also help with
- * eventual mistakes.
+ * After the user selecting the option to initiate the user story, a window with
+ * the available connected instances will appear. The user will then choose the 
+ * desired one and the system will request him for the name of the workbook to 
+ * be searched. The user will introduce it and the system will present if the 
+ * workbook was found or not. If it was found the system will also present a small
+ * summary with the name of the spreadsheets and some of their cells content.
  *
+ * <b>Notes:</b><p>
+ * Class Cleansheet: This class is responsible for getting the open workbooks.
+ * <p>
+ * Class Spreadsheet: This class is responsible for getting the title of the
+ * spreadsheet and the content of it's cells to present in the summary to the user.
+ * <p>
+ * Class UIExtensionSearchWorkbook: This class is responsible to build the extensions
+ * menu that the user will select when he wants to performed the user story.
+ * <p>
+ * Class CtrlExtensionSearchWorkbook: This class is responsible for controlling the
+ * flow of the user story.
+ * <p>
+ * Class CommTCPClientsManager: This class is responsible to hold the instances 
+ * that are connected.
+ * <p>
+ * 
  * <h2>4. Open Questions</h2>
- * <ul>
+ * <ol>
  * <li>Where should it be located the button of this functionally?</li>
  * <li>How many values of cells of each worksheet must be shown? (Columns &
  * Rows)</li>
@@ -43,16 +61,20 @@
  * the user be warned or both workbooks be opened?</li>
  * <li>What should happen if the workbook has no spreadsheets?</li>
  * <li>What should happen if the spreasheet has no content?</li>
- * </ul>
+ * </ol>
  * 
- * <h2>5. Assumptions</h2>
- * <ul>
- * <li>The button for this functionally should be in the "Extensions" Menu.</li>
- * <li>If the workbook has no spreadsheets the user should be warned.</li>
- * <li>If the spreadsheet has no content the user should be warned.</li>
+ * <h2>5. Answers</h2>
+ * 2. For each worksheet it should be shown the first 3 non-empty cells on the 
+ * summary presented to the user.<p>
+ *
+ * <h2>6. Assumptions</h2>
+ * 1. The button for this functionally should be in the "Extensions" Menu.<p>
+ * 3. It is not possible to have two workbooks with the same name.<p>
+ * 4. If the workbook has no spreadsheets the user should be warned.<p>
+ * 5. If the spreadsheet has no content the user should be warned.<p>
  * </ul>
- * 
- * <h2>6. Tests</h2>
+ *
+ * <h2>7. Tests</h2>
  *
  * <b>Test1:</b> verifyWorkbookIsOpenInRemoteInstanceTest<p>
  * There should be a way to verify if the workbook found by the name that the
@@ -63,6 +85,8 @@
  * There should be at least a spreadsheet with cells not empty.
  * <p>
  * 
+ * 
+ *
  * @author Miguel Silva (1150901)
  */
-package lapr4.green.s1.ipc.n1150901.search.instance;
+package lapr4.green.s1.ipc.n1150901.search.workbook;
--- src/test/java/lapr4/green/s1/ipc/n1150901/search/workbook/SearchWorkbookTest.java
+++ src/test/java/lapr4/green/s1/ipc/n1150901/search/workbook/SearchWorkbookTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package lapr4.green.s1.ipc.n1150901.search.instance;
+package lapr4.green.s1.ipc.n1150901.search.workbook;
 
 import csheets.CleanSheets;
 import csheets.core.Address;
