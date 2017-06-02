73b67b07d32e93898c31c585aad741b5040ef851
Diogo Alexandre Vasconcelos Santos <1150451@isep.ipp.pt>
2017-06-02 10:12:47 +0100

    LAPR4E17DD-143 #time 23min #comment added base ExportPDF Class

---
+++ src/main/java/lapr4/red/s1/core/n1150451/exportPDF/application/ExportPDFController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr4.red.s1.core.n1150451.exportPDF.application;
+
+/**
+ *
+ * @author Diogo Santos
+ */
+public class ExportPDFController {
+    
+}
