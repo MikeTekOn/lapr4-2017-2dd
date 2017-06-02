48cf50db86a8b9f07d64034a37d9a86db9d4c2ab
EricAmaral <1141570@isep.ipp.pt>
2017-06-02 12:42:46 +0100

    LAPR4E17DD-167 #time 1h 30min #comment updates export xml

--- src/main/java/lapr4/blue/s1/lang/n1141570/XML/application/ExportXML.java
+++ src/main/java/lapr4/blue/s1/lang/n1141570/XML/application/ExportXML.java
@@ -1,8 +1,11 @@
 package lapr4.blue.s1.lang.n1141570.XML.application;
 
 import csheets.core.Cell;
+import csheets.core.IllegalValueTypeException;
 import java.io.File;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -25,7 +28,7 @@ public class ExportXML implements ExportStrategy {
     private static final int ROOT_VALUE = 0;
     private static final int ELEMENT1_VALUE = 1;
     private static final int ELEMENT2_VALUE = 2;
-    
+
     private String path;
     private List<Cell> cellsList;
     private List<String> tagNamesList;
@@ -39,6 +42,7 @@ public class ExportXML implements ExportStrategy {
 
     /**
      * Defines the selected range of cells.
+     *
      * @param cellsList the selected list of cells.
      */
     public void selectRange(List<Cell> cellsList) {
@@ -46,7 +50,9 @@ public class ExportXML implements ExportStrategy {
     }
 
     /**
-     * Configures tag names by attributing the received list of names to the tag names.
+     * Configures tag names by attributing the received list of names to the tag
+     * names.
+     *
      * @param tagNamesList the received list of tag names.
      */
     public void configureTagNames(List<String> tagNamesList) {
@@ -60,6 +66,7 @@ public class ExportXML implements ExportStrategy {
 
     /**
      * Receives the selected path where to save the xml file.
+     *
      * @param path the chosen path.
      */
     public void selectPath(String path) {
@@ -68,11 +75,15 @@ public class ExportXML implements ExportStrategy {
 
     /**
      * Exports the data to xml file.
+     *
      * @return returns true if exports, false otherwise.
      */
     @Override
     public boolean export() {
+
+        int i = 0;
         boolean success = true;
+
         try {
             DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder docBuilder = null;
@@ -84,13 +95,30 @@ public class ExportXML implements ExportStrategy {
             Element rootElement = doc.createElement(tagNamesList.get(ROOT_VALUE)); //basically to give the proper name do the root tag
             doc.appendChild(rootElement);
 
-            //worksheet element
-            Element spreadsheetElement = doc.createElement(tagNamesList.get(ELEMENT1_VALUE));
-            rootElement.appendChild(spreadsheetElement);
+            while (i != cellsList.size()) {
+                boolean flag = true;
+                //worksheet elements
+                Element spreadsheetElement = doc.createElement(tagNamesList.get(ELEMENT1_VALUE));
+                rootElement.appendChild(spreadsheetElement);
+                Cell tempCell = cellsList.get(i);
+                String currentSpreadSheetCellTitle = tempCell.getSpreadsheet().getTitle();
 
-            //cell element
-            Element cellElement = doc.createElement(tagNamesList.get(ELEMENT2_VALUE));
-            spreadsheetElement.appendChild(cellElement);
+                while (flag) {
+                    Cell currentCell = cellsList.get(i);
+                    //cell element
+                    Element cellElement = doc.createElement(tagNamesList.get(ELEMENT2_VALUE));
+                    spreadsheetElement.appendChild(cellElement);
+                    try {
+                        cellElement.appendChild(doc.createTextNode(currentCell.getValue().toText()));
+                    } catch (IllegalValueTypeException ex) {
+                        Logger.getLogger(ExportXML.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    if (!currentSpreadSheetCellTitle.equalsIgnoreCase(currentCell.getSpreadsheet().getTitle())) {
+                        flag = false;
+                    }
+                    i++;
+                }
+            }
 
             // write the content into xml file
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
