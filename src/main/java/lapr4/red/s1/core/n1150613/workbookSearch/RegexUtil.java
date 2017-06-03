07cb42a832fada4d12bb10f3949baebd81c967ab
Diogo Guedes <1150613@isep.ipp.pt>
2017-06-03 18:59:48 +0100

    LAPR4E17DD-146 #comment fixes

--- src/main/java/lapr4/red/s1/core/n1150613/workbookSearch/RegexUtil.java
+++ src/main/java/lapr4/red/s1/core/n1150613/workbookSearch/RegexUtil.java
@@ -5,6 +5,11 @@
  */
 package lapr4.red.s1.core.n1150613.workbookSearch;
 
+import csheets.core.Cell;
+import csheets.core.Spreadsheet;
+import csheets.core.Workbook;
+import java.util.Iterator;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
@@ -15,12 +20,13 @@ import java.util.regex.PatternSyntaxException;
  */
 public class RegexUtil {
 
-    private String regExpression;
+    private List<String> info;
+    private String regex;
     private Pattern p;
     private Matcher m;
 
     public RegexUtil(String regex) {
-        this.regExpression = regex;
+        this.regex = regex;
     }
 
     /**
@@ -31,7 +37,7 @@ public class RegexUtil {
     public boolean isRegexValid() {
         boolean test = true;
         try {
-            Pattern.compile(regExpression);
+            Pattern.compile(regex);
             System.out.println("Valid");
         } catch (PatternSyntaxException exception) {
             System.out.println("Invalid!");
@@ -49,9 +55,48 @@ public class RegexUtil {
      */
     public boolean checkIfMatches(String cellContent) {
 
-        p = Pattern.compile(regExpression);
+        p = Pattern.compile(regex);
         m = p.matcher(cellContent);
         return (m.matches());
+    }
+
+    /**
+     * The call to the method to search through the whole woorkbook and match
+     * it's contents with the inserted regular expression.
+     *
+     * @param w
+     *
+     * @return desired cells information in String array
+     *
+     */
+    public List<String> checkifRegexMatches(Workbook w) {
+        Spreadsheet s;
+        Cell c;
+        String content;
+        int cont = 0;
+
+        if (!isRegexValid()) {
+            return null;
+        }
+
+        Iterator<Spreadsheet> it = w.iterator();
+        Iterator<Cell> itCell;
+
+        while (it.hasNext()) {
+            s = it.next();
+            cont++;
+            itCell = s.iterator();
+            while (itCell.hasNext()) {
+                c = itCell.next();
+                content = c.getValue().toString();
+                if (checkIfMatches(content)) {
+                    content = content.substring(0, Math.min(content.length(), 10));
+                    info.add(content + " Spreadsheet:" + cont + " Adress:" + c.getAddress().toString());
+                }
+            }
+        }
+
+        return info;
     }
 
 }
