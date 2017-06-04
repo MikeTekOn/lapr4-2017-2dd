d66603e33a952c219090ef012f0b06085aabc0e9
EricAmaral <1141570@isep.ipp.pt>
2017-06-04 17:12:03 +0100

    LAPR4E17DD-167 #time 2h #comment updates ui functionalities

--- src/main/java/csheets/ui/ExportToMenu.java
+++ src/main/java/csheets/ui/ExportToMenu.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package csheets.ui;
 
 import csheets.CleanSheets;
@@ -11,10 +6,8 @@ import csheets.SpreadsheetAppListener;
 import csheets.ui.ctrl.ActionManager;
 import csheets.ui.ctrl.UIController;
 import java.awt.event.KeyEvent;
-import java.io.File;
 import javax.swing.ImageIcon;
 import javax.swing.JMenu;
-import lapr4.red.s1.core.n1150451.exportPDF.presentation.ExportToPDFAction;
 
 /**
  *
@@ -33,15 +26,15 @@ public class ExportToMenu extends JMenu implements SpreadsheetAppListener {
         // Stores members
         this.app = app;
         this.uiController = uiController;
-        this.actionManager=actionManager;
+        this.actionManager = actionManager;
 
         // Configures menu
         app.addSpreadsheetAppListener(this);
         setMnemonic(KeyEvent.VK_E);
         setIcon(new ImageIcon(CleanSheets.class.getResource("res/img/reopen.gif")));
 
-
         add(actionManager.getAction("PDF"));
+        add(new ExportToXmlSubMenu(actionManager));
     }
 
     public void workbookCreated(SpreadsheetAppEvent event) {
