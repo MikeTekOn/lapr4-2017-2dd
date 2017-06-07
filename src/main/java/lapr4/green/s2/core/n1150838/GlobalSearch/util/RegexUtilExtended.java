a93cb9d675116827ec395c558f8ce71b7b8de079
Nuno Pinto <1150838@isep.ipp.pt>
2017-06-07 16:02:28 +0100

    LAPR4E17DD-203 #time 4h design is almost finished

--- src/main/java/lapr4/green/s2/core/n1150838/GlobalSearch/util/RegexUtilExtended.java
+++ src/main/java/lapr4/green/s2/core/n1150838/GlobalSearch/util/RegexUtilExtended.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package lapr4.green.s2.core.n1150838.GlobalSearch;
+package lapr4.green.s2.core.n1150838.GlobalSearch.util;
 
 import csheets.core.Workbook;
 import java.util.Stack;
