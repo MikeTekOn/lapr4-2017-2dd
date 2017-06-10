26fb408b8b589062fbab5c9883b7c32b5751a12d
Pedro Fernandes <1060503@isep.ipp.pt>
2017-06-11 00:27:16 +0100

    LAPR4E17DD-267 #time 1h #comment create userChatProfileUI

--- src/main/java/lapr4/blue/s2/ipc/n1060503/chat/ChangeUserChatProfileController.java
+++ src/main/java/lapr4/blue/s2/ipc/n1060503/chat/ChangeUserChatProfileController.java
@@ -5,15 +5,51 @@
  */
 package lapr4.blue.s2.ipc.n1060503.chat;
 
+import java.io.IOException;
+import javax.swing.ImageIcon;
+import lapr4.blue.s2.ipc.n1060503.chat.profile.StatusChatProfile;
+import lapr4.blue.s2.ipc.n1060503.chat.profile.UserChatProfile;
+
 /**
  *
  * @author Pedro Fernandes
  */
 public class ChangeUserChatProfileController {
     
+    UserChatProfile ucp;
     
-    public ChangeUserChatProfileController(){
+    public ChangeUserChatProfileController() throws IOException{
+        // TODO get from repository
+        ucp = new UserChatProfile();
+    }
+    
+    public String getUsername(){
+        return ucp.getUsername();
+    }
+    
+    public String getNickname(){
+        return ucp.getNickname();
+    }
+    
+    public String getStatus(){
+        return ucp.getStatus().toString();
+    }
+    
+    public ImageIcon getIcon(){
+        return ucp.getImage();
+    }
+    
+    public boolean changeInfo(String nickname, 
+            StatusChatProfile status) throws IOException{
         
+        ucp.changeInfo(nickname, status);
+        
+        //TODO save repository
+        return ucp != null;
+    }
+    
+    public void changeIcon(String icon_path) throws IOException{
+        ucp.changeIcon(icon_path);
     }
     
 }
