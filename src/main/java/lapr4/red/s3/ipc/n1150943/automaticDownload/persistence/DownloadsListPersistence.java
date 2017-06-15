97d40a60b4715bd321f60dbd02672015b7166370
João Cardoso <1150943@isep.ipp.pt>
2017-06-15 04:36:21 +0100

    LAPR4E17DD-279 #time 4h #comment Created logic of implementation on code
    Needs to add the update downloads section and finish UI as well as a lot of testing

---
+++ src/main/java/lapr4/red/s3/ipc/n1150943/automaticDownload/persistence/DownloadsListPersistence.java
@@ -0,0 +1,111 @@
+package lapr4.red.s3.ipc.n1150943.automaticDownload.persistence;
+
+import lapr4.red.s3.ipc.n1150943.automaticDownload.DownloadInfo;
+
+import java.io.*;
+import java.util.Map;
+
+/**
+ * Created by João Cardoso on 14-06-2017.
+ */
+public class DownloadsListPersistence {
+
+    public static void saveList(Map<String,DownloadInfo> downloads){
+        try{
+            FileOutputStream fileOut = new FileOutputStream(System.getProperty("user.dir"));
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(downloads);
+            out.close();
+            fileOut.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Map<String,DownloadInfo> getDownloads(){
+        Map<String,DownloadInfo> downloads = null;
+        try{
+            FileInputStream fileIn = new FileInputStream(System.getProperty("user.dir"));
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            downloads = (Map<String,DownloadInfo>) in.readObject();
+            in.close();
+            fileIn.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return downloads;
+    }
+
+    public static DownloadInfo getLatestVersionInfo(String originalFileName) {
+        Map<String,DownloadInfo> downloads = getDownloads();
+        Map<String,DownloadInfo> fileVersions = getDownloads();
+        DownloadInfo originalInfo = downloads.get(originalFileName);
+        if(originalInfo.downloadType() == DownloadInfo.DownloadType.ONE_TIME_DOWNLOAD){
+            return originalInfo;
+        }else{
+            if(originalInfo.updateType()==DownloadInfo.UpdateType.RENAME){
+                return originalInfo;
+            }
+        }
+        for (Map.Entry<String,DownloadInfo> e : downloads.entrySet()){
+            if(e.getValue().originalFileName().equals(originalFileName)){
+                fileVersions.put(e.getKey(),e.getValue());
+            }
+        }
+        int latestVersion = 1;
+        DownloadInfo latestVersionInfo = null;
+        for (DownloadInfo downloadInfo : fileVersions.values()){
+            int version = downloadInfo.version();
+            if(version>=latestVersion){
+                latestVersionInfo = downloadInfo;
+            }
+        }
+        if(latestVersionInfo==null){
+            System.err.println("Couldn't find any version of the file");
+        }
+        return latestVersionInfo;
+    }
+
+    /**
+     * Gets the original name of the file if it has versions
+     * @param fileName
+     * @return
+     */
+    public static String getOriginalFileName (String fileName) {
+        return getDownloads().get(fileName).originalFileName();
+    }
+
+    public static String getLatestVersion(String fileName) {
+        Map<String,DownloadInfo> downloads = getDownloads();
+        Map<String,DownloadInfo> fileVersions = getDownloads();
+        DownloadInfo originalInfo = downloads.get(fileName);
+        if(originalInfo.downloadType() == DownloadInfo.DownloadType.ONE_TIME_DOWNLOAD){
+            return fileName;
+        }else{
+            if(originalInfo.updateType()==DownloadInfo.UpdateType.RENAME){
+                return fileName;
+            }
+        }
+        for (Map.Entry<String,DownloadInfo> e : downloads.entrySet()){
+            if(e.getValue().originalFileName().equals(fileName)){
+                fileVersions.put(e.getKey(),e.getValue());
+            }
+        }
+        int latestVersion = 1;
+        String latestVersionName = fileName;
+        for (Map.Entry<String,DownloadInfo> e : fileVersions.entrySet()){
+            int version = e.getValue().version();
+            if(version>=latestVersion){
+                latestVersionName = e.getKey();
+            }
+        }
+
+        return latestVersionName;
+    }
+}
