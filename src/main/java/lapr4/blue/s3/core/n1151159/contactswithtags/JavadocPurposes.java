5ffad744a6d0dfacd469da03a3d52ce3e78ed80e
ivoferroo <1151159@isep.ipp.pt>
2017-06-15 12:15:45 +0100

    LAPR4E17DD-301 #resolve #comment makes the FI analysis

---
+++ src/main/java/lapr4/blue/s3/core/n1151159/contactswithtags/JavadocPurposes.java
@@ -0,0 +1,16 @@
+package lapr4.blue.s3.core.n1151159.contactswithtags;
+
+/**
+ * This class exists in order to generate the javadoc documentation for this package.
+ *
+ * @author IvoFerro
+ */
+public final class JavadocPurposes {
+
+    /**
+     * Private empty constructor to prevent the instantiation of this class.
+     */
+    private JavadocPurposes() {
+    }
+
+}
